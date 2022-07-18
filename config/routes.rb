3c61afca3309a50f6188c099638a7ce9a38892b1
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2022-07-18 16:27:01 -0700

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :participations, only: [:index, :show, :create, :destroy]
+  resources :events
+  resources :users
+  resources :categories, only: [:index, :show, :create]
+  
+  post '/signup', to: 'users#create'
+  get '/me', to: 'users#show'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  # Routing logic: fallback requests for React Router.
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
