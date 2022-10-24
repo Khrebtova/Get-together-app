c10a66cf65006c55935fef4dbf6ce3d60e2c668f
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2022-10-24 15:24:15 -0700

    adjust navbar style for smaller devices

--- client/src/components/Navigation/NavBar.jsx
+++ client/src/components/Navigation/NavBar.jsx
@@ -2,7 +2,8 @@ import React, { useContext } from 'react'
 import {useNavigate, Link} from 'react-router-dom'
 import {UserContext} from '../context/user'
 import {AppBar, Typography, Button, Divider, MenuItem, Menu, Avatar, Tooltip, IconButton, Stack} from '@mui/material';
-import Logo from '../../assets/logo-white.jpg'
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import SearchIcon from '@mui/icons-material/Search';
 
 const Navbar = () => {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -20,12 +21,20 @@ const Navbar = () => {
   const loggedInLinks = () => {
     const letter = user.username.slice(0,1).toUpperCase()   
     return(
-      <>        
-        <Button color="inherit" onClick={()=>navigate('/events')}>Browse all events</Button>
-        <Button color="inherit" onClick={()=>navigate('/events/new')}>Create new Event</Button>
+      <> 
+        <Tooltip title="Browse Events" placement="bottom">       
+          <Button color="inherit" size='small' onClick={()=>navigate('/events')}>
+            <SearchIcon sx={{fontSize: {lg: '30px', md: '20px', sm: '20px'}}} /> 
+          </Button>
+        </Tooltip>
+        <Tooltip title="Create Event" placement="bottom">
+          <Button color="inherit" onClick={()=>navigate('/events/new')}>
+            <AddCircleOutlineIcon sx={{fontSize: {lg: '30px', md: '20px', sm: '20px'}}}/> 
+          </Button>
+        </Tooltip>  
         <div>
           <Tooltip title="menu">
-            <IconButton
+            <IconButton              
               size="large"
               aria-label="account of current user"
               aria-controls="menu-appbar"
@@ -33,7 +42,7 @@ const Navbar = () => {
               onClick={handleMenu}
               color="inherit"
             >
-              <Avatar sx={{ width: 42, height: 42, bgcolor: 'white', color: 'purple', fontFamily: 'monospace', fontWeight: 700}}>{letter}</Avatar>
+              <Avatar sx={{ width: {lg: 50, sm: 30}, height: {lg: 50, sm: 30}, bgcolor: '#F6F6F6', color: '#212121', fontWeight: 900}}>{letter}</Avatar>
             </IconButton>
           </Tooltip>
           <Menu
@@ -48,10 +57,13 @@ const Navbar = () => {
               vertical: 'top',
               horizontal: 'right',
             }}
+            fontSize='small'
             open={Boolean(anchorEl)}
             onClose={handleClose}
           >
             <MenuItem onClick={()=>{navigate('/my-events'); handleClose()}}>My Events</MenuItem>
+            <MenuItem onClick={()=>{navigate('/events'); handleClose()}}>Browse Events</MenuItem>
+            <MenuItem onClick={()=>{navigate('/events/new'); handleClose()}}>Create Event</MenuItem>
             <MenuItem onClick={()=> {navigate('/logout'); handleClose()}}>Logout</MenuItem>
           </Menu>
         </div>
@@ -69,11 +81,16 @@ const Navbar = () => {
   }
 
   return (      
-      <AppBar position="fixed" sx={{background: '#6D9886', height: '120px'}}>
-        <Stack direction="row" alignItems='center' p='20px' ml='40px' mr='40px'>       
-          <img src={Logo} alt="logo" className='logo' style={{height: '80px', borderRadius: '20px'}} onClick={()=>navigate('/')}/>
+      <AppBar position="fixed" sx={{background: '#6D9886', height: '120px', p: {lg: '20px 40px', md: '40px', sm: '30px 40px', xs: '20px'}}}>
+        <Stack direction='row' alignItems='center'  >           
           <Typography                         
-            sx={{ flexGrow: 1, mr: 2, ml: 2,  fontSize: {lg: '40px', sm: '30px'}, fontWeight: 700, letterSpacing: '.3rem', color: 'inherit'}} 
+            sx={{ 
+            flexGrow: 1,
+            fontSize: {lg: '40px', md: '40px', sm: '30px', xs: '20px'}, 
+            fontWeight: 700, 
+            letterSpacing: '.3rem', 
+            color: 'inherit'
+            }} 
             onClick={()=>navigate('/')}
           >
             GET TOGETHER
